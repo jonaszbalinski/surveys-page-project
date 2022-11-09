a6f8df222d6e161d2e03f54a1cbc9f8b7d7d5a7b
Jonasz Balinski <jonasz.balinski@wp.pl>
2022-11-09 17:25:30 +0100

    Update main buttons

--- front/js/script.js
+++ front/js/script.js
@@ -1,7 +1,12 @@
 window.onload = animateText();
+window.onload = document.getElementById("menu_button").addEventListener("click",menuHide);
+window.onload = document.getElementById("user_button").addEventListener("click",userHide);
+
 var id = null;
 var textToDisplay = "Working in progress...";
-
+var menuVisible = false;
+var userVisible = false;
+		
 function animateText() 
 {
 	var mainText = document.getElementById("main_text");
@@ -21,4 +26,42 @@ function animateText()
 		}
 		else textIterator = 0;
 	}
+}
+
+function menuHide()
+{
+		var menuBox = document.getElementById("menu_box");
+
+		if(menuVisible)
+		{
+			menuBox.style.visibility = "hidden";
+			menuVisible = false;
+			
+			
+		}
+		else
+		{
+			menuBox.style.visibility = "visible";
+			menuVisible = true
+			
+		}
+}
+
+function userHide()
+{
+		var userBox = document.getElementById("user_box");
+
+		if(userVisible)
+		{
+			userBox.style.visibility = "hidden";
+			userVisible = false;
+			
+			
+		}
+		else
+		{
+			userBox.style.visibility = "visible";
+			userVisible = true
+			
+		}
 }
