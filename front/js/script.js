c6a3e4ccd5f663d20032599754915c1beff1db9f
Jonasz Baliński <100127755+jonaszbalinski@users.noreply.github.com>
2022-11-09 17:48:55 +0100

    Add login/reigster/about us pages (#8)
    * New css animations
    
    * Update main buttons
    
    * Add basic navigation

--- front/js/script.js
+++ front/js/script.js
@@ -3,10 +3,31 @@ window.onload = document.getElementById("menu_button").addEventListener("click",
 window.onload = document.getElementById("user_button").addEventListener("click",userHide);
 
 var id = null;
-var textToDisplay = "...working_in_progress.......";
+var textToDisplay = "Working in progress...";
 var menuVisible = false;
 var userVisible = false;
 		
+function animateText() 
+{
+	var mainText = document.getElementById("main_text");
+	var textIterator = 0;
+	clearInterval(id);
+	id = setInterval(frame, 200);
+	
+	function frame() 
+	{
+		if (textIterator != textToDisplay.length)
+		{
+			textIterator++;
+			mainText.innerText = textToDisplay.substring(0, textIterator);
+			mainText.style.color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+			mainText.style.fontSize = (Math.floor(Math.random() * 5) + 98) + "px";
+
+		}
+		else textIterator = 0;
+	}
+}
+
 function menuHide()
 {
 		var menuBox = document.getElementById("menu_box");
@@ -45,31 +66,6 @@ function userHide()
 		}
 }
 
-
-
-function animateText() 
-{
-	var mainText = document.getElementById("main_text");
-	var textIterator = 0;
-	clearInterval(id);
-	id = setInterval(frame, 100);
-	
-	
-	function frame() 
-	{
-		if (textIterator != textToDisplay.length)
-		{
-			textIterator++;
-			mainText.innerText = "";
-			for (var i = 0; i < textIterator; i++)
-			{
-				mainText.innerText += textToDisplay[i];
-				mainText.style.color = '#' + Math.floor(Math.random() * 16777215).toString(16);
-				mainText.style.fontSize = (Math.floor(Math.random() * 2) + 26) + "px";
-			}
-		}
-		else textIterator = 0;
-	}
-
-	
-}
+const validateLogin = () => {
+	alert('!');
+  }
