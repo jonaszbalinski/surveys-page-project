632381f43e49872d254833f9ce9cf405b1332051
adamk17 <100127469+adamk17@users.noreply.github.com>
2022-11-21 13:24:06 +0100

    Add about us page, add continue without login button and change some pages look (#12)
    Co-authored-by: adamk17 <adam,kucharski17@gmail.com>

--- front/js/script.js
+++ front/js/script.js
@@ -1,23 +1,21 @@
 window.onload = animateText();
-window.onload = document.getElementById("menu_button").addEventListener("click",menuHide);
-window.onload = document.getElementById("user_button").addEventListener("click",userHide);
+window.onload = document.getElementById("menu_button").addEventListener("click", menuHide);
+window.onload = document.getElementById("user_button").addEventListener("click", userHide);
+
 
 var id = null;
 var textToDisplay = "Working in progress...";
 var menuVisible = false;
 var userVisible = false;
-		
-function animateText() 
-{
+
+function animateText() {
 	var mainText = document.getElementById("main_text");
 	var textIterator = 0;
 	clearInterval(id);
 	id = setInterval(frame, 200);
-	
-	function frame() 
-	{
-		if (textIterator != textToDisplay.length)
-		{
+
+	function frame() {
+		if (textIterator != textToDisplay.length) {
 			textIterator++;
 			mainText.innerText = textToDisplay.substring(0, textIterator);
 			mainText.style.color = '#' + Math.floor(Math.random() * 16777215).toString(16);
@@ -28,44 +26,34 @@ function animateText()
 	}
 }
 
-function menuHide()
-{
-		var menuBox = document.getElementById("menu_box");
+function menuHide() {
+	var menuBox = document.getElementById("menu_box");
 
-		if(menuVisible)
-		{
-			menuBox.style.visibility = "hidden";
-			menuVisible = false;
-			
-			
-		}
-		else
-		{
-			menuBox.style.visibility = "visible";
-			menuVisible = true
-			
-		}
+	if (menuVisible) {
+		menuBox.style.visibility = "hidden";
+		menuVisible = false;
+	}
+	else {
+		menuBox.style.visibility = "visible";
+		menuVisible = true
+	}
+
+
 }
 
-function userHide()
-{
-		var userBox = document.getElementById("user_box");
+function userHide() {
+	var userBox = document.getElementById("user_box");
 
-		if(userVisible)
-		{
-			userBox.style.visibility = "hidden";
-			userVisible = false;
-			
-			
-		}
-		else
-		{
-			userBox.style.visibility = "visible";
-			userVisible = true
-			
-		}
+	if (userVisible) {
+		userBox.style.visibility = "hidden";
+		userVisible = false;
+	}
+	else {
+		userBox.style.visibility = "visible";
+		userVisible = true
+	}
 }
 
 const validateLogin = () => {
 	alert('!');
-  }
+}
