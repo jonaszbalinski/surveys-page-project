849d534b985b812cea17f0398ac82bfe10dd7d27
adamk17 <100127469+adamk17@users.noreply.github.com>
2023-01-09 20:14:26 +0100

    Feature/deploy main page (#15)
    * Add about us page, add continue without login button and change some pages look
    
    * Correct login page
    
    * Something
    
    * Update login.js
    
    Co-authored-by: adamk17 <adam,kucharski17@gmail.com>
    Co-authored-by: Jonasz Baliński <100127755+jonaszbalinski@users.noreply.github.com>

--- front/js/login.js
+++ front/js/login.js
@@ -1,3 +1,7 @@
+let validEmail = false;
+let validPassword = false;
+let validPassword2 = false;
+let validUser = false;
 function setFormMessage(formElement, type, message) {
     const messageElement = formElement.querySelector(".form__message");
 
@@ -27,9 +31,11 @@ function validateEmail(email) {
 function validatePassword(password) {
     var validRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$/;
   
-    if (password.match(validRegex))
-        return true;
-    return false;
+    //if (password.match(validRegex))
+        //return true;
+    //return false;
+    
+    return true;
 }
 
 function loginDataIsValid() {
@@ -73,35 +79,58 @@ document.addEventListener("DOMContentLoaded", () =>{
         inputElement.addEventListener("blur", e => {
             if (e.target.id === "loginEmail") {
                 if (e.target.value.length < 5 || !validateEmail(e.target.value)) {
+                    validEmail = false;
                     setInputError(inputElement, "Please enter vaild email");
+                }
+                else{
+                    validEmail = true;
                 }
             }
             else if (e.target.id === "loginPassword") {
-                
+                validPassword = true;
             }
             else if (e.target.id === "createAccountUsername") {
                 if (e.target.value.length < 4 || e.target.value.length > 20) {
+                    validUser = false;
                     setInputError(inputElement, "Username must contain between 4 and 20 characters");
+                }
+                else{
+                    validUser = true;
                 }
             }
             else if (e.target.id === "createAccountEmail") {
                 if (e.target.value.length < 5 || !validateEmail(e.target.value)) {
+                    validEmail = false;
                     setInputError(inputElement, "Please enter vaild email");
+                }
+                else{
+                    validEmail = true;
                 }
             }
             else if (e.target.id === "createAccountPassword") {
                 if (e.target.value.length < 6 || e.target.value.length > 20) {
-                    setInputError(inputElement, "Password must contain between 6 and 20 characters");
+                    validPassword = false;
+                    setInputError(inputElement, "Password must contain between 6 and 20 characters" );
                 }
+                
                 else if (!validatePassword(e.target.value)) {
+                    validPassword = false;
                     setInputError(inputElement, "Password must contain at least one numeric digit, one uppercase and one lowercase letter");
+                }
+                else{
+                    validPassword = true;
                 }
             }
             else if (e.target.id === "createAccountConfirmPassword") {
                 if (!(e.target.value === document.querySelector("#createAccountPassword").value)) {
+                    validPassword2 = false;
                     setInputError(inputElement, "Passwords does not match")
                 }
+                else{
+                    validPassword2 = true;
+                }
             }
+            
         });
 
         inputElement.addEventListener("input", e => {
@@ -111,15 +140,21 @@ document.addEventListener("DOMContentLoaded", () =>{
 
     document.querySelector("#loginContinue").addEventListener("click", e => {
         e.preventDefault();
-        if (loginDataIsValid()) {
+        if (validEmail  && validPassword) {
+        //if (loginDataIsValid()) {
             // ...
+            
+            location.href="main_page.html";
         }
     });
 
     document.querySelector("#createAccountContinue").addEventListener("click", e => {
         e.preventDefault();
-        if (createAccountDataIsValid()) {
+        if (validEmail && validPassword && validPassword2 && validUser) {
+        //if (createAccountDataIsValid()) {
             // ...
+            
+            location.href="main_page.html";
         }
     });
 });
